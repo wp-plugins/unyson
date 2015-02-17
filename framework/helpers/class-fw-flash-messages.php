db070cf4c1907ac9a3f5425ee9a220e9a38eebe3
Unyson <Unyson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-17 14:51:38 +0000

    tagging version 2.1.24
    git-svn-id: https://plugins.svn.wordpress.org/unyson/tags/2.1.24@1092484 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- framework/helpers/class-fw-flash-messages.php
+++ framework/helpers/class-fw-flash-messages.php
@@ -23,7 +23,7 @@ class FW_Flash_Messages
 	{
 		$messages = FW_Session::get(self::$session_key);
 
-		if (!is_array($messages)) {
+		if (empty($messages) || !is_array($messages)) {
 			$messages = array_fill_keys(array_keys(self::$available_types), array());
 		}
 
@@ -140,6 +140,7 @@ class FW_Flash_Messages
 
 	/**
 	 * Use this method to print messages html in frontend
+	 * @return bool If some html was printed or not
 	 */
 	public static function _print_frontend()
 	{
@@ -148,30 +149,55 @@ class FW_Flash_Messages
 		$html = array_fill_keys(array_keys(self::$available_types), '');
 		$all_messages = self::get_messages();
 
+		$messages_exists = false;
+
 		foreach ($all_messages as $type => $messages) {
-			if (!empty($messages)) {
-				foreach ($messages as $id => $data) {
-					$html[$type] .= '<li class="fw-flash-message">'. nl2br($data['message']) .'</li>';
-
-					unset($all_messages[$type][$id]);
-				}
-
-				$html[$type] = '<ul class="fw-flash-type-'. $type .'">'. $html[$type] .'</ul>';
+			if (empty($messages)) {
+				continue;
 			}
+
+			foreach ($messages as $id => $data) {
+				$html[$type] .= '<li class="fw-flash-message">'. nl2br($data['message']) .'</li>';
+
+				unset($all_messages[$type][$id]);
+			}
+
+			$html[$type] = '<ul class="fw-flash-type-'. $type .'">'. $html[$type] .'</ul>';
+
+			$messages_exists = true;
 		}
 
 		self::set_messages($all_messages);
 
-		echo '<div class="fw-flash-messages">';
-		echo implode("\n\n", $html);
-		echo '</div>';
-
 		self::$frontend_printed = true;
+
+		if ($messages_exists) {
+			echo '<div class="fw-flash-messages">';
+			echo implode("\n\n", $html);
+			echo '</div>';
+
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public static function _frontend_printed()
 	{
 		return self::$frontend_printed;
+	}
+
+	public static function _get_messages($clear = false)
+	{
+		self::process_pending_remove_ids();
+
+		$messages = self::get_messages();
+
+		if ($clear) {
+			self::set_messages(array());
+		}
+
+		return $messages;
 	}
 }
 
@@ -211,7 +237,9 @@ if (is_admin()) {
 			return;
 		}
 
-		FW_Flash_Messages::_print_frontend();
+		if (!FW_Flash_Messages::_print_frontend()) {
+			return;
+		}
 
 		echo
 		'<script type="text/javascript">'.
